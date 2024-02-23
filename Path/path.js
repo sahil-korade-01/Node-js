e00a08ad787561a7c451e04076b5af5d57ba4644
sahil-korade-01 <sahilkorade3@gmail.com>
2024-02-23 10:17:01 +0530

    path modules

---
+++ Path/path.js
@@ -0,0 +1,15 @@
+//   path modules  
+
+
+
+
+
+const path = require('path')
+// console.log(path.dirname(Path/path.js));
+
+console.log(path.extname('C:/Users/91935/Desktop/BackEnd/Node js/Path/path.js'));
+console.log(path.parse('C:/Users/91935/Desktop/BackEnd/Node js/Path/path.js'));
+
+ const myPath = path.parse('C:/Users/91935/Desktop/BackEnd/Node js/Path/path.js')
+
+console.log(myPath.name);
