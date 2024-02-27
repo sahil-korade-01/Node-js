6e170eee3fce17a616b8f55c5d1b50a53edd414b
sahil-korade-01 <sahilkorade3@gmail.com>
2024-02-27 16:36:16 +0530

    json data add on node file

--- Express/nav.js
+++ Express/nav.js
@@ -1,30 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 4000;
 
+app.get("/", (request, responce) => {
+  responce.send("Hello Guys This is the git welcome page");
+});
 
+app.get("/about", (request, responce) => {
+  responce.write("<h1>This is the about page</h1>");
+  responce.write("<h1>Hello ata closed krun dakhva</h1>");
+  responce.send();
+});
 
-const express = require("express")
-const app = express()
-const port = 4000
+app.get("/contact", (request, responce) => {
+  responce.send("this is contact page");
+});
 
+app.get("/login", (request, responce) => {
+  responce.json([
+    {
+      name: "sahil",
+      id: 99,
+      age: 23,
+    },
+    {
+      name: "chima",
+      id: 333,
+      age: 23,
+    },
+  ]);
+});
 
-app.get("/",(request,responce)=>{
-    responce.send("Hello Guys This is the git welcome page")
-})
-
-app.get("/about",(request,responce)=>{
-    responce.send("This is the about page")
-})
-
-app.get("/contact",(request,responce)=>{
-    responce.send("this is the contact page")
-
-})
-
-app.get("/login",(request,responce)=>{
-    responce.send("this is the login page")
-})
-
-app.listen(`${port}`,()=>{
-    console.log("the listining number is 4000");
-})
-
-
+app.listen(`${port}`, () => {
+  console.log("the listining number is 4000");
+});
